10a6077131be71970b81409a88efa37afe7bb879
Gabriel Dalpiaz <gabriel666dalpiaz@gmail.com>
2024-03-11 10:41:51 -0300

    first commit

---
+++ src/app/components/small-card/small-card.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit,Input } from '@angular/core';
+
+@Component({
+  selector: 'app-small-card',
+  templateUrl: './small-card.component.html',
+  styleUrls: ['./small-card.component.css']
+})
+export class SmallCardComponent implements OnInit {
+  @Input()
+  photoCover:string =''
+  @Input()
+  cardTitle:string = ''
+  @Input()
+  cardDescription:string = ''
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
